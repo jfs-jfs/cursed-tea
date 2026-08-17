564dfa2c82e98b777fc003612aa18e7e5c483c05
Jacob <jacob.folch@gmail.com>
2026-08-17 21:24:38 +0200

    fix:canvas-write:cwrite_char_line didn't work on diagonal lines, now it does but looks ugly as fuck

--- src/core/canvas-write.c
+++ src/core/canvas-write.c
@@ -2,10 +2,11 @@
 #include <cursed-tea/logger.h>
 #include <cursed-tea/style/brush.h>
 #include <cursed-tea/uvector.h>
+#include <math.h>
 #include <stddef.h>
 
 void ct_cwrite_char(struct CtCanvas *canvas, const size_t x, const size_t y,
-                 const wchar_t ch) {
+                    const wchar_t ch) {
   CtUVector point = {x, y};
   CtUVector canvas_area = {canvas->max_x, canvas->max_y};
   if (!ct_uvec_is_inside(canvas_area, point)) {
@@ -16,26 +17,37 @@ void ct_cwrite_char(struct CtCanvas *canvas, const size_t x, const size_t y,
 }
 
 void _ct_cwrite_char_rec(struct CtCanvas *canvas, const CtUVector from,
-                      const CtUVector to, const wchar_t character) {
+                         const CtUVector to, const wchar_t character) {
   ct_brush_cell(&canvas->cell_matrix[from.y][from.x], character);
   if (ct_uvec_same_as(from, to)) {
     return;
   }
 
-  float distance1 = ct_uvec_distance((CtUVector){from.x + 1, from.y}, to);
-  float distance2 = ct_uvec_distance((CtUVector){from.x, from.y + 1}, to);
+  size_t smallest_idx = 0;
+  size_t smallest_distance = MAXFLOAT;
+  float distances[] = {.0, .0, .0, .0};
+  CtUVector directions[] = {
+      {from.x + 1, from.y},
+      {from.x, from.y + 1},
+      {from.x - 1, from.y},
+      {from.x, from.y - 1},
+  };
 
-  if (distance1 < distance2) {
-    _ct_cwrite_char_rec(canvas, (CtUVector){from.x + 1, from.y}, to, character);
-    return;
-  }
+  for (size_t i = 0; i < 4; i++)
+    distances[i] = ct_uvec_distance(directions[i], to);
 
-  _ct_cwrite_char_rec(canvas, (CtUVector){from.x, from.y + 1}, to, character);
+  for (size_t i = 0; i < 4; i++)
+    if (distances[i] < smallest_distance) {
+      smallest_distance = distances[i];
+      smallest_idx = i;
+    }
+
+  _ct_cwrite_char_rec(canvas, directions[smallest_idx], to, character);
 }
 
 void ct_cwrite_char_line(struct CtCanvas *canvas, const size_t from_x,
-                      const size_t from_y, const size_t to_x, const size_t to_y,
-                      const wchar_t ch) {
+                         const size_t from_y, const size_t to_x,
+                         const size_t to_y, const wchar_t ch) {
 
   CtUVector start = {from_x, from_y};
   CtUVector end = {to_x, to_y};
@@ -56,14 +68,15 @@ void ct_cfill(struct CtCanvas *canvas, const wchar_t character) {
 }
 
 void ct_cwrite(struct CtCanvas *canvas, const size_t x, const size_t y,
-            const wchar_t *text) {
+               const wchar_t *text) {
   size_t text_length = wcslen(text);
   CtUVector start = {x, y};
   CtUVector end = {x + text_length, y};
   CtUVector canvas_area = {canvas->max_x, canvas->max_y};
 
   // start is a cell; end.x is exclusive (end.y is the same cell row).
-  if (!ct_uvec_is_inside(canvas_area, start) || !ct_uvec_fits(canvas_area, end)) {
+  if (!ct_uvec_is_inside(canvas_area, start) ||
+      !ct_uvec_fits(canvas_area, end)) {
     log_error(L"Trying to write outside of canvas!");
     return;
   }
@@ -73,14 +86,15 @@ void ct_cwrite(struct CtCanvas *canvas, const size_t x, const size_t y,
 }
 
 void ct_cwritev(struct CtCanvas *canvas, const size_t x, const size_t y,
-             const wchar_t *text) {
+                const wchar_t *text) {
   size_t text_length = wcslen(text);
   CtUVector start = {x, y};
   CtUVector end = {x, y + text_length};
   CtUVector canvas_area = {canvas->max_x, canvas->max_y};
 
   // start is a cell; end.y is exclusive (end.x is the same cell column).
-  if (!ct_uvec_is_inside(canvas_area, start) || !ct_uvec_fits(canvas_area, end)) {
+  if (!ct_uvec_is_inside(canvas_area, start) ||
+      !ct_uvec_fits(canvas_area, end)) {
     log_error(L"Trying to write outside of canvas!");
     return;
   }
@@ -161,7 +175,8 @@ void ct_cwrite_ee(struct CtCanvas *canvas, const wchar_t *text) {
 
 // Vertical center
 //
-void ct_cwritev_c(struct CtCanvas *canvas, const size_t x, const wchar_t *text) {
+void ct_cwritev_c(struct CtCanvas *canvas, const size_t x,
+                  const wchar_t *text) {
   size_t text_length = wcslen(text);
   size_t y = (canvas->max_y - text_length) / 2;
   ct_cwritev(canvas, x, y, text);
@@ -187,7 +202,8 @@ void ct_cwritev_ce(struct CtCanvas *canvas, const wchar_t *text) {
 
 // Vertical start
 //
-void ct_cwritev_s(struct CtCanvas *canvas, const size_t x, const wchar_t *text) {
+void ct_cwritev_s(struct CtCanvas *canvas, const size_t x,
+                  const wchar_t *text) {
   ct_cwritev(canvas, x, 0, text);
 }
 
@@ -205,7 +221,8 @@ void ct_cwritev_se(struct CtCanvas *canvas, const wchar_t *text) {
 
 // Vertical end
 //
-void ct_cwritev_e(struct CtCanvas *canvas, const size_t x, const wchar_t *text) {
+void ct_cwritev_e(struct CtCanvas *canvas, const size_t x,
+                  const wchar_t *text) {
   size_t text_length = wcslen(text);
   size_t y = canvas->max_y - text_length;
   ct_cwritev(canvas, x, y, text);

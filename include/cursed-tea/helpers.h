dd80623e518bd49a8b40dc119caa2ce6ff415670
Jacob <jacob.folch@gmail.com>
2026-09-19 14:33:48 +0200

    refactor:added helper functions before provided by ui/commons.h

--- include/cursed-tea/helpers.h
+++ include/cursed-tea/helpers.h
@@ -1,7 +1,13 @@
 #pragma once
 
+#include "cursed-tea/canvas.h"
+#include "cursed-tea/event.h"
 #include <cursed-tea.h>
 
 inline void ct_subrender(const struct CtModel *model, struct CtCanvas *canvas);
 inline void ct_subhandler(struct CtModel *model, const struct CtEvent *event);
 inline void ct_subcleanup(struct CtModel *model);
+
+void ct_empty_cleanup(void *uncasted_model);
+void ct_empty_handler(void *uncasted_model, const struct CtEvent *event);
+void ct_empty_render(const void *uncasted_model, struct CtCanvas *canvas);
--- src/helpers.c
+++ src/helpers.c
@@ -9,3 +9,15 @@ inline void ct_subhandler(struct CtModel *model, const struct CtEvent *event) {
 inline void ct_subrender(const struct CtModel *model, struct CtCanvas *canvas) {
   model->render(model, canvas);
 }
+
+void ct_empty_cleanup(void *uncasted_model) { (void)uncasted_model; }
+
+void ct_empty_handler(void *uncasted_model, const struct CtEvent *event) {
+  (void)uncasted_model;
+  (void)event;
+}
+
+void ct_empty_render(const void *uncasted_model, struct CtCanvas *canvas) {
+  (void)uncasted_model;
+  (void)canvas;
+}

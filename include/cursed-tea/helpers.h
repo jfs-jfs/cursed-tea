5fd2284044e7326a44c769815155adc7344aa585
Jacob <jacob.folch@gmail.com>
2026-09-19 14:50:10 +0200

    revert:no need to optimize this right now

--- include/cursed-tea/helpers.h
+++ include/cursed-tea/helpers.h
@@ -4,9 +4,9 @@
 #include "cursed-tea/event.h"
 #include <cursed-tea.h>
 
-inline void ct_subrender(const struct CtModel *model, struct CtCanvas *canvas);
-inline void ct_subhandler(struct CtModel *model, const struct CtEvent *event);
-inline void ct_subcleanup(struct CtModel *model);
+void ct_subrender(const struct CtModel *model, struct CtCanvas *canvas);
+void ct_subhandler(struct CtModel *model, const struct CtEvent *event);
+void ct_subcleanup(struct CtModel *model);
 
 void ct_empty_cleanup(void *uncasted_model);
 void ct_empty_handler(void *uncasted_model, const struct CtEvent *event);
--- src/helpers.c
+++ src/helpers.c
@@ -1,12 +1,12 @@
 #include <cursed-tea/helpers.h>
 
-inline void ct_subcleanup(struct CtModel *model) { model->cleanup(model); }
+void ct_subcleanup(struct CtModel *model) { model->cleanup(model); }
 
-inline void ct_subhandler(struct CtModel *model, const struct CtEvent *event) {
+void ct_subhandler(struct CtModel *model, const struct CtEvent *event) {
   model->handler(model, event);
 }
 
-inline void ct_subrender(const struct CtModel *model, struct CtCanvas *canvas) {
+void ct_subrender(const struct CtModel *model, struct CtCanvas *canvas) {
   model->render(model, canvas);
 }
 
